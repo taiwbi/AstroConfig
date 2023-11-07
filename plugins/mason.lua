e6a13e181e6464d9727c33ee33e439c125961c89
taiwbi <admin@taiwbi.com>
2023-11-07 17:22:28 +0330

    Added Python and PHP Denugger Configured PHP Debugger
    + Added blade formatter and syntax highlighter but currently it doesn't
      work

--- plugins/mason.lua
+++ plugins/mason.lua
@@ -29,6 +29,8 @@ return {
         -- Web Development
         "prettier",
         "phpcbf",
+        "blade-formatter",
+        "tlint", -- blade linter
         -- python
         "black",
       })
@@ -40,8 +42,45 @@ return {
     opts = function(_, opts)
       -- add more things to the ensure_installed table protecting against community packs modifying it
       opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
-        -- "python",
+        -- Web Development
+        "php",
+        -- Python
+        "python",
       })
+      opts.handlers = {
+        python = function(source_name)
+          local dap = require "dap"
+          dap.adapters.php = {
+            type = "executable",
+            command = "node",
+            args = {
+              "/home/mahdi/.local/share/nvim/mason/packages/php-debug-adapter/extension/out/phpDebug.js",
+            },
+          }
+
+          dap.configurations.php = {
+            {
+              type = "php",
+              request = "launch",
+              name = "Listen for Xdebug",
+              port = "9003",
+            },
+            {
+              name = "Launch currently open script",
+              type = "php",
+              request = "launch",
+              program = "${file}",
+              cwd = "${fileDirname}",
+              port = 0,
+              runtimeArgs = { "-dxdebug.start_with_request=yes" },
+              env = {
+                XDEBUG_MODE = "debug,develop",
+                XDEBUG_CONFIG = "client_port=${port}",
+              },
+            },
+          }
+        end,
+      }
     end,
   },
 }
