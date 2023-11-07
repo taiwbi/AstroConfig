76abc2be2ebf2803945477b5c3fe0859722bff1c
taiwbi <admin@taiwbi.com>
2023-11-07 19:07:19 +0330

    Added scrolloff

--- options.lua
+++ options.lua
@@ -7,6 +7,7 @@ return {
     spell = false, -- sets vim.opt.spell
     signcolumn = "auto", -- sets vim.opt.signcolumn to auto
     wrap = false, -- sets vim.opt.wrap
+    scrolloff = 14,
   },
   g = {
     mapleader = " ", -- sets vim.g.mapleader
