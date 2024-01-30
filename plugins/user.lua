92bd23faf73a32c096e35e914cdb34765f7fbd02
taiwbi <admin@taiwbi.com>
2024-01-30 16:09:16 +0330

    Added Tabnine for AI completion 😀

--- plugins/user.lua
+++ plugins/user.lua
@@ -88,4 +88,20 @@ return {
     opts = {},
     event = "User AstroFile",
   },
+  {
+    "codota/tabnine-nvim",
+    build = "./dl_binaries.sh",
+    config = function()
+      require("tabnine").setup {
+        disable_auto_comment = true,
+        accept_keymap = "<S-Tab>",
+        dismiss_keymap = "<C-[>",
+        debounce_ms = 400,
+        suggestion_color = { gui = "#4a4e56", cterm = 244 },
+        exclude_filetypes = { "TelescopePrompt", "neo-tree" },
+        log_file_path = nil, -- absolute path to Tabnine log file
+      }
+    end,
+    event = "User AstroFile",
+  },
 }
