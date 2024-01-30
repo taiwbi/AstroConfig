c023a9e1de54696510028a4f06fd53c223f3c0e3
taiwbi <admin@taiwbi.com>
2024-01-30 19:39:03 +0330

    Removed the stupid laravel snippet that didn't have any prefix and caused the tab button to insert a stupid annoying confirm message instead of tab. Why would anyone add a snippet without a prefix? :/

--- plugins/luasnip.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "L3MON4D3/LuaSnip",
-  dependencies = { "rafamadriz/friendly-snippets" },
-  config = function(_, opts)
-    require("luasnip").config.setup(opts)
-    require("luasnip.loaders.from_vscode").lazy_load()
-    require("luasnip.loaders.from_vscode").lazy_load { paths = { "./lua/user/snippets/laravel" } }
-  end,
-}
