de2d8903bcbc38e01377fc7cae7732d3a0d1d7f0
taiwbi <admin@taiwbi.com>
2024-01-30 17:26:42 +0330

    Changed accept keymap for tabnine to <C-]>

--- plugins/user.lua
+++ plugins/user.lua
@@ -94,10 +94,10 @@ return {
     config = function()
       require("tabnine").setup {
         disable_auto_comment = true,
-        accept_keymap = "<S-Tab>",
+        accept_keymap = "<C-]>",
         dismiss_keymap = "<C-[>",
         debounce_ms = 400,
-        suggestion_color = { gui = "#4a4e56", cterm = 244 },
+        suggestion_color = { gui = "#2b2b2b", cterm = 150 },
         exclude_filetypes = { "TelescopePrompt", "neo-tree" },
         log_file_path = nil, -- absolute path to Tabnine log file
       }
